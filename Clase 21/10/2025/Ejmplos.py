8181202db78ddfbfc2b5d21eaa5ac8b24ba4858d
winston <wuistonesneiderrodri@gmail.com>
2025-10-22 19:21:46 -0500

    add: excercises

---
+++ Clase 21/10/2025/Ejmplos.py
@@ -0,0 +1,19 @@
+# EJERCICIO 1
+"""
+
+"""
+
+#EJERCICIO 2
+
+# 1 Galon = 3.785 Litros
+# 3.785 Litros = 1 Galon
+
+galon = 3.785
+
+cantidadLitros = int(input("Ingrese la cantidad de leche producida en un dia (Litros): "))
+
+total = cantidadLitros/galon
+
+print(f"Usted recibira: {total}")
+
+
