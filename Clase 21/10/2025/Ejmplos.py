605d7c82ce061580d69140b6cc94c422ec5bf0a3
winston <wuistonesneiderrodri@gmail.com>
2025-10-22 20:55:48 -0500

    add: excecise #4 - calcular total a pagar de los platos por persona

--- Clase 21/10/2025/Ejmplos.py
+++ Clase 21/10/2025/Ejmplos.py
@@ -24,8 +24,7 @@ print(f"Usted recibira: {total:.2f}")
 
 
 # EJERCICIO 3
-
-
+"""
 valorTraje = float(input("Ingrese el valor de su compra: "))
 nombre = input("Ingrese su nombre por favor: ")
 documento = int(input("Ingrese su documento por favor:"))
@@ -37,4 +36,28 @@ if valorTraje > 2500.00:
     descuento = valorTraje * 0.15
     total = valorTraje - descuento
     print(f"----- FACTURA ----- \n El cliente: {nombre} \n identificado con documento: {documento} \n en la fecha de: {fecha} \n pago un total de: {total} \n con un descueto de: {descuento}")
+
+"""
+
+#EJERCICIO 4 
+
+plato = 95.00
+
+anfitrion = input("¿como te llamas?: ")
+cantidadPersonas = int(input("¿Cuantas personas asistiran al evento?"))
+fecha = datetime.now()
+operacion = plato * cantidadPersonas
+
+if cantidadPersonas > 200 or cantidadPersonas <= 300:
+    plato = 85.00
+    operacion
+    print(f"----- FACTURA ----- \n en la fecha: {fecha} \n se registra el evento a nombre de: {anfitrion} \n con una cantidad de personas de: {cantidadPersonas} \n un valor por unidad del plato de: {plato} \n y un valor a pagar de: {operacion}")
+elif cantidadPersonas > 300:
+    plato = 75.00
+    operacion
+    print(f"----- FACTURA ----- \n en la fecha: {fecha} \n se registra el evento a nombre de: {anfitrion} \n con una cantidad de personas de: {cantidadPersonas} \n un valor por unidad del plato de: {plato} \n y un valor a pagar de: {operacion}")
+else:
+    print("cantidad de personas invalido")
+
+
 
