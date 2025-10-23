93dc3f4d01de992cacc7afac1205a2ff944a9602
winston <wuistonesneiderrodri@gmail.com>
2025-10-22 20:31:52 -0500

    add: excercise 3 - Descuento trajes

--- Clase 21/10/2025/Ejmplos.py
+++ Clase 21/10/2025/Ejmplos.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # EJERCICIO 1
 """
 
@@ -8,12 +10,31 @@
 # 1 Galon = 3.785 Litros
 # 3.785 Litros = 1 Galon
 
+"""
+
 galon = 3.785
 
 cantidadLitros = int(input("Ingrese la cantidad de leche producida en un dia (Litros): "))
 
 total = cantidadLitros/galon
 
-print(f"Usted recibira: {total}")
+print(f"Usted recibira: {total:.2f}")
 
+"""
+
+
+# EJERCICIO 3
+
+
+valorTraje = float(input("Ingrese el valor de su compra: "))
+nombre = input("Ingrese su nombre por favor: ")
+documento = int(input("Ingrese su documento por favor:"))
+fecha = datetime.now()
+
+
+
+if valorTraje > 2500.00:
+    descuento = valorTraje * 0.15
+    total = valorTraje - descuento
+    print(f"----- FACTURA ----- \n El cliente: {nombre} \n identificado con documento: {documento} \n en la fecha de: {fecha} \n pago un total de: {total} \n con un descueto de: {descuento}")
 
